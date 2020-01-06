3dbb68bea61467c937c7780e9057c5d890eabb8a
Tomasz Lektorowski <tomasz.lektorowski@gmail.com>
2020-01-06 16:10:04 +0100

    updated technologies section
    added click and hover event handling over specific card

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let navbarSelector = document.querySelector("nav");
+let technologyItemList = document.querySelectorAll(".technologies__item");
 
 
 function fixedNavbar() {
@@ -11,11 +12,23 @@ function fixedNavbar() {
     }
 }
 
+function technologyItemActive() {
+    console.log(technologyItemList);
+    technologyItemList.forEach(item => {
+        item.addEventListener('click', event => {
+            let technologyItemFront = item.firstElementChild;
+            let technologyItemBack = item.lastElementChild;
+            technologyItemFront.classList.toggle("technologies__item--front--active");
+            technologyItemBack.classList.toggle("technologies__item--back--active");
+        })
+    })
+}
+
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
     window.addEventListener("scroll", () => {
         fixedNavbar();
     });
-
+    technologyItemActive();
 });
