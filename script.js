f011773bc214a9b706c246553af31e242825c1cd
Tomasz Lektorowski <tomasz.lektorowski@gmail.com>
2020-01-07 09:03:25 +0100

    competed footer and imporved portfolio section, added mobile navigation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let navbarSelector = document.querySelector("nav");
 let technologyItemList = document.querySelectorAll(".technologies__item");
+let portfolioItemList = document.querySelectorAll(".portfolio__item");
+let navTogglerSelector = document.querySelector(".nav__toggler");
+let navItemsSelector = document.querySelectorAll(".nav__list--item");
 
 
 function fixedNavbar() {
@@ -13,13 +16,39 @@ function fixedNavbar() {
 }
 
 function technologyItemActive() {
-    console.log(technologyItemList);
     technologyItemList.forEach(item => {
         item.addEventListener('click', event => {
             let technologyItemFront = item.firstElementChild;
             let technologyItemBack = item.lastElementChild;
             technologyItemFront.classList.toggle("technologies__item--front--active");
             technologyItemBack.classList.toggle("technologies__item--back--active");
+        })
+    })
+}
+
+function navMobile() {
+    navTogglerSelector.addEventListener("click", () => {
+        navTogglerSelector.classList.toggle("nav__toggler--active");
+    });
+}
+
+
+function portfolioItemActive() {
+    portfolioItemList.forEach(item => {
+        item.addEventListener('click', event => {
+            let portfolioItemFront = item.firstElementChild;
+            let portfolioItemBack = item.lastElementChild;
+            portfolioItemFront.classList.toggle("portfolio__item--front--active");
+            portfolioItemBack.classList.toggle("portfolio__item--back--active");
+        })
+    })
+}
+
+
+function closeMobileMenuOnClick() {
+    navItemsSelector.forEach(item => {
+        item.addEventListener('click', event => {
+            navTogglerSelector.classList.remove("nav__toggler--active");
         })
     })
 }
@@ -31,4 +60,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
         fixedNavbar();
     });
     technologyItemActive();
-});
+    portfolioItemActive();
+    closeMobileMenuOnClick()
+    navMobile();
+}
+);
